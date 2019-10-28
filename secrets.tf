68cfe13bfc4d3983fc1a31d50f2196758aecfda6
Mikael Michelier <mikael.michelier@googlemail.com>
2019-10-28 14:55:53 +0000

    resource docker_service wordpress-service {

--- secrets.tf
+++
@@ -1,9 +0,0 @@
-resource "docker_secret" "mysql_root_password" {
-  name = "root_password"
-  data = "${base64encode(var.mysql_root_password)}"
-}
-
-resource "docker_secret" "mysql_db_password" {
-  name = "db_password"
-  data = "${base64encode(var.mysql_user_password)}"
-}
--- vars.tf
+++ vars.tf
@@ -1,11 +1,11 @@
 variable "mysql_root_password" {
   description = "The MySQL root password."
-  default     = "MTIzNDU2Cg=="
+  default     = "123456"
 }
 
 variable "mysql_user_password" {
   description = "The MySQL user password."
-  default     = "MTIzNDU2Cg=="
+  default     = "123456"
 }
 
 variable "wordpress_db_username" {
